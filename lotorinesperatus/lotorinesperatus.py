16a993255973c2415bb8aca52f3bd3b9e51168d2
Nicklas Boman <smurfd@gmail.com>
2025-10-10 23:07:30 +0200

    lotorinesperatus, added menu if needed

--- lotorinesperatus/lotorinesperatus.py
+++ lotorinesperatus/lotorinesperatus.py
@@ -60,6 +60,26 @@ class LotorInesperatus:
     prgstr = '{:03}'.format(progress * 100 // pmax)
     stdscr.addstr(2, 3, '[' + '-' * filled + ' ' * (width - filled) + ']' +  f' {prgstr}%', curses.color_pair(3))
 
+  def curses_menu(self): # TODO if needed
+    menu_items = ["Option 1", "Option 2", "Option 3"]
+    selected = 0
+    while True:
+      stdscr.clear()
+      for i, item in enumerate(menu_items):
+        if i == selected: stdscr.addstr(i, 0, f"> {item}", curses.A_REVERSE)
+        else: stdscr.addstr(i, 0, item)
+      key = stdscr.getch()
+      if key == curses.KEY_UP and selected > 0:
+        selected -= 1
+      elif key == curses.KEY_DOWN and selected < len(menu_items) - 1:
+        selected += 1
+      elif key == 10:  # Enter key
+        stdscr.addstr(len(menu_items) + 1, 0, f"You selected: {menu_items[selected]}")
+        stdscr.refresh()
+        stdscr.getch()
+        break
+
+
   def cwin(self, stdscr):
     bindat = self.get_binary()
     self.curses_setup(curses, stdscr)
