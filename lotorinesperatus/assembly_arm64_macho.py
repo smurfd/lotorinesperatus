a4ada8d62bd4333d6b0e592c333fa61ee029e108
Nicklas Boman <smurfd@gmail.com>
2025-11-07 16:41:02 +0100

    lotorinesperatus, append lists

--- lotorinesperatus/assembly_arm64_macho.py
+++ lotorinesperatus/assembly_arm64_macho.py
@@ -8,33 +8,45 @@
 # https://yossarian.net/res/pub/macho-internals/macho-internals.pdf
 class Arm64_macho:
   def __init__(self, fn):
-    self.header = [b'', b'', b'', b'', b'', b'', b'', b'']
-    self.loader = [b'', b'', b'', b'', b'', b'', b'', b'', b'', b'', b'']
+    self.header, self.loader, self.data, self.fn = [], [], [], fn
     hl, ll = self.get_lengths()
-    self.fn = fn
-    with open(self.fn, 'rb') as f: self.h, self.l = f.read(hl), f.read(ll)
-  def get_lengths(self): return 32, 72   # header, loader
-  def get_header(self):             # [::-1] for big endian
-    self.header[0] = self.h[0:4]    # Magic number
-    self.header[1] = self.h[4:8]    # CPU type
-    self.header[2] = self.h[8:12]   # CPU subtype
-    self.header[3] = self.h[12:16]  # Filetype
-    self.header[4] = self.h[16:20]  # Number of load commands
-    self.header[5] = self.h[20:24]  # Size of load commands
-    self.header[6] = self.h[24:28]  # Flags
-    self.header[7] = self.h[28:32]  # Reserved. 64bit only
+    with open(self.fn, 'rb') as f: self.h, self.l, self.d = f.read(hl), f.read(ll), f.read()
+  def get_lengths(self): return 32, 72             # header, loader
+  def get_header(self):                            # [::-1] for big endian
+    self.header.append(self.h[0:4])                # Magic number
+    self.header.append(self.h[4:8])                # CPU type
+    self.header.append(self.h[8:12])               # CPU subtype
+    self.header.append(self.h[12:16])              # Filetype
+    self.header.append(self.h[16:20])              # Number of load commands
+    self.header.append(self.h[20:24])              # Size of load commands
+    self.header.append(self.h[24:28])              # Flags
+    self.header.append(self.h[28:32])              # Reserved. 64bit only
     return self.header
   def get_loader(self):
-    self.loader[0] = self.l[0:4]    # Command type
-    self.loader[1] = self.l[4:8]    # Command size
-    self.loader[2] = self.l[8:24]   # Segment name
-    self.loader[3] = self.l[24:32]  # Address
-    self.loader[4] = self.l[32:40]  # Address size
-    self.loader[5] = self.l[40:48]  # File offset
-    self.loader[6] = self.l[48:56]  # Size (bytes from file offset)
-    self.loader[7] = self.l[56:60]  # Maximum virtual memory protection
-    self.loader[8] = self.l[60:64]  # Initial virtual memory protection
-    self.loader[9] = self.l[64:68]  # Number of sections
-    self.loader[10]= self.l[68:72]  # Flags32
+    self.loader.append(self.l[0:4])                # Command type
+    self.loader.append(self.l[4:8])                # Command size
+    self.loader.append(self.l[8:24])               # Segment name
+    self.loader.append(self.l[24:32])              # Address
+    self.loader.append(self.l[32:40])              # Address size
+    self.loader.append(self.l[40:48])              # File offset
+    self.loader.append(self.l[48:56])              # Size (bytes from file offset)
+    self.loader.append(self.l[56:60])              # Maximum virtual memory protection
+    self.loader.append(self.l[60:64])              # Initial virtual memory protection
+    self.loader.append(self.l[64:68])              # Number of sections
+    self.loader.append(self.l[68:72])              # Flags32
     return self.loader
+  def get_data(self):
+    self.data = self.d
+    return self.data
 
+"""
+>>> s = "68656c6c6f20776f726c64"
+>>> res = ''.join([chr(int(s[i:i+2], 16)) for i in range(0, len(s), 2)])
+>>> res
+'hello world'
+
+
+b = [b'\xcf\xfa\xed\xfe', b'\x0c\x00\x00\x01', b'\x00\x00\x00\x00', b'\x02\x00\x00\x00', b'\x11\x00\x00\x00', b' \x04\x00\x00', b'\x85\x00 \x00', b'\x00\x00\x00\x00', b'\x19\x00\x00\x00', b'H\x00\x00\x00', b'__PAGEZERO\x00\x00\x00\x00\x00\x00', b'\x00\x00\x00\x00\x00\x00\x00\x00', b'\x00\x00\x00\x00\x01\x00\x00\x00', b'\x00\x00\x00\x00\x00\x00\x00\x00', b'\x00\x00\x00\x00\x00\x00\x00\x00', b'\x00\x00\x00\x00', b'\x00\x00\x00\x00', b'\x00\x00\x00\x00', b'\x00\x00\x00\x00']
+>>> for b1 in b:
+...     print(binascii.hexlify(b1))
+"""
