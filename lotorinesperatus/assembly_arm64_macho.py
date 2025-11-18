e53137a72e1c5fb0e445636afb3f263587fadaa8
Nicklas Boman <smurfd@gmail.com>
2025-11-18 19:38:10 +0100

    lotorinesperatus, macho get sections

--- lotorinesperatus/assembly_arm64_macho.py
+++ lotorinesperatus/assembly_arm64_macho.py
@@ -13,12 +13,15 @@ import binascii
 # https://yossarian.net/res/pub/macho-internals/macho-internals.pdf
 class Arm64_macho:
   def __init__(self, fn) -> None:
-    self.header, self.command, self.loader, self.data, self.segment, self.fn = [], [], [], [], [], fn
+    self.header, self.command, self.loader, self.data, self.segment, self.sections, self.sections_data, self.fn = [], [], [], [], [], [], [], fn
     hl, ll, sl = self.get_lengths()
     with open(self.fn, 'rb') as f:
-      self.h = f.read(hl)
-      self.c, self.l, self.s, self.d = f.read(int(f'{binascii.hexlify(self.get_big(self.h[20:24])).decode()}', 16)), f.read(ll), f.read(sl), f.read()
-    #with open(self.fn, 'rb') as f: self.h, self.c, self.l, self.s, self.d = f.read(hl), f.read(cl), f.read(ll), f.read(sl), f.read()
+      self.file = f.read(); p1, p2 = 0, hl
+      self.h = self.file[p1:p2]; p1, p2 = hl, hl + int(f'{binascii.hexlify(self.get_big(self.h[20:24])).decode()}', 16)
+      self.c = self.file[p1:p2]; p1, p2 = p2, p2 + ll
+      self.l = self.file[p1:p2]; p1, p2 = p2, p2 + sl
+      self.s = self.file[p1:p2]
+      self.d = self.file[p2:]
   def get_big(self, s) -> List: return s[::-1]
   def get_lengths(self) -> Tuple:
     return 32, 72, 80                              # Lenght of header, loader, segment
@@ -34,41 +37,16 @@ class Arm64_macho:
     return self.header
   def get_command(self) -> List:
     p1, p2 = 0, 4
-    # TODO: dont just print, save to variables
-    # TODO: make into functions
     for i in range(int(f'{binascii.hexlify(self.get_big(self.h[16:20])).decode()}', 16)):
       nr = self.get_big(self.c[p1:p2])
-      nrr = int(f'{binascii.hexlify(nr).decode()}', 16)
-      nsec = 0
-      if (nrr == 25): #0x19
+      nrr, nsec = int(f'{binascii.hexlify(nr).decode()}', 16), 0
+      if (nrr == 25): #0x19, meaning it has sections
         loa = self.get_loader(p1)
-        nsec = int(f'{binascii.hexlify(self.get_big(loa[9])).decode()}', 16)
-        print(f'cmd loa {i}: {loa}')
-        print(f'cmd loa {i}: SECs {nsec}')
-        s1 = p1 + 72  # 72 for size of loader
-        print('------')
-        for j in range(nsec):
-          sec = self.get_segment(s1+(j * 80))
-          print(f'cmd loa sec {i} {j}: {sec}')
-          pos = self.get_big(sec[4])
-          siz = self.get_big(sec[3])
-          print(f'cmd loa sec {i} {j}: {pos} {siz}')
-          pos = int(f'{binascii.hexlify(pos).decode()}', 16)
-          siz = int(f'{binascii.hexlify(siz).decode()}', 16)
-          print(f'cmd loa sec {i} {j}: {pos} {siz}')
-          with open(self.fn, 'rb') as f: # TODO: read this in __init__?
-            f.seek(pos)  # find position for data in file
-            d = f.read(siz)  # read data size
-            print(f'cmd loa data {i} {j}: {d}')
-        print('------')
+        s1 = p1 + 72  # 72 is the size of the loader
+        self.get_sections(int(f'{binascii.hexlify(self.get_big(loa[9])).decode()}', 16), s1)
       p1, p2 = p1 + 4, p2 + 4
-      sz = self.get_big(self.c[p1:p2])
-      sz = int(f'{binascii.hexlify(sz).decode()}', 16)
-      print(f'cmd {i}: {nr}')
-      print(f'cmd {i}: {sz}')  # size (including the 4 + 4)
-      p1, p2 = p2, p2 + sz - 8
-      print(f'cmd {i}: {self.c[p1:p2]}')
-      p1, p2 = p2, p2 + 4
+      sz = int(f'{binascii.hexlify(self.get_big(self.c[p1:p2])).decode()}', 16)
+      p1, p2 = p2 + sz - 8, p2 + sz - 8 + 4
   def get_loader(self, c) -> List:
     self.loader = []
     self.loader.append(self.c[c + 0:c + 4])        # Command type
@@ -85,19 +63,29 @@ class Arm64_macho:
     return self.loader
   def get_segment(self, c) -> List:
     self.segment = []
-    self.segment.append(self.c[c + 0:c + 16])              # Section name
-    self.segment.append(self.c[c + 16:c + 32])             # Segment name
-    self.segment.append(self.c[c + 32:c + 40])             # Section address
-    self.segment.append(self.c[c + 40:c + 48])             # Section size
-    self.segment.append(self.c[c + 48:c + 52])             # Section file offset
-    self.segment.append(self.c[c + 52:c + 56])             # Alignment
-    self.segment.append(self.c[c + 56:c + 60])             # Relocations file offset
-    self.segment.append(self.c[c + 60:c + 64])             # Number of relocations0
-    self.segment.append(self.c[c + 64:c + 68])             # Flag/type
-    self.segment.append(self.c[c + 68:c + 72])             # Reserved1
-    self.segment.append(self.c[c + 72:c + 76])             # Reserved2
-    self.segment.append(self.c[c + 76:c + 80])             # Reserved3
+    self.segment.append(self.c[c + 0:c + 16])      # Section name
+    self.segment.append(self.c[c + 16:c + 32])     # Segment name
+    self.segment.append(self.c[c + 32:c + 40])     # Section address
+    self.segment.append(self.c[c + 40:c + 48])     # Section size
+    self.segment.append(self.c[c + 48:c + 52])     # Section file offset
+    self.segment.append(self.c[c + 52:c + 56])     # Alignment
+    self.segment.append(self.c[c + 56:c + 60])     # Relocations file offset
+    self.segment.append(self.c[c + 60:c + 64])     # Number of relocations0
+    self.segment.append(self.c[c + 64:c + 68])     # Flag/type
+    self.segment.append(self.c[c + 68:c + 72])     # Reserved1
+    self.segment.append(self.c[c + 72:c + 76])     # Reserved2
+    self.segment.append(self.c[c + 76:c + 80])     # Reserved3
     return self.segment
+  def get_sections(self, nr, p) -> List:
+    for i in range(nr):
+      sec = self.get_segment(p + (i * 80))
+      self.sections.append(sec)
+      print(f'-- cmd loa sec {i}: {sec}')
+      pos, siz = int(f'{binascii.hexlify(self.get_big(sec[4])).decode()}', 16), int(f'{binascii.hexlify(self.get_big(sec[3])).decode()}', 16)
+      print(f'----- cmd loa sec {i}: {pos} {siz}')
+      self.sections_data.append(self.file[pos:pos + siz])
+      print(f'----- cmd loa data {i}: {self.file[pos:pos + siz]}')
+    return self.sections, self.sections_data
   def get_data(self) -> bytes:
     self.data = self.d
     return self.data
