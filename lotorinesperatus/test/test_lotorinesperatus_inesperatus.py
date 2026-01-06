972cecde10d19048a6de101cc5260aa6b0955427
Nicklas Boman <smurfd@gmail.com>
2026-01-06 21:31:34 +0100

    lotorinesperatus, dissasamblin'. func binary

--- lotorinesperatus/test/test_lotorinesperatus_inesperatus.py
+++ lotorinesperatus/test/test_lotorinesperatus_inesperatus.py
@@ -4,7 +4,7 @@
 from lotorinesperatus.assembly import Assembly
 import platform, sys, os
 
-def read_arm_macho() -> None:
+def read_arm_macho_hello() -> None:
   print('--- arm64 macho header ---')
   arm = Assembly(f'{os.path.dirname(os.path.realpath(__file__))}/examples/hello_arm64_macho.bin', arch='arm64', flavour='arm64', binfmt='macho')
   arm_header  = arm.asm.get_header()
@@ -20,9 +20,9 @@ def read_arm_macho() -> None:
   [print(f'Loader {arm.hex2str(arm_loader[i])}') for i in range(len(arm_loader))]
   [print(f'Segment {arm.hex2str(arm_segment[i])}') for i in range(len(arm_segment))]
   h, bi, a, b = arm.asm.get_assembly()
-  [print(f'Arm Asm: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
+  [print(f'Arm Asm hello: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
   print('--- arm64 macho header ---')
-def read_amd_elf() -> None:
+def read_amd_elf_hello() -> None:
   print('--- amd64 elf header ---')
   amd = Assembly(f'{os.path.dirname(os.path.realpath(__file__))}/examples/hello_amd64_elf.bin', arch='amd64', flavour='amd64', binfmt='elf')
   amd_header         = amd.asm.get_header()
@@ -37,11 +37,48 @@ def read_amd_elf() -> None:
   [print(f'Program {amd.hex2str(amd_header_program[i])}') for i in range(len(amd_header_program))]
   [print(f'Section {amd.hex2str(amd_header_section[i])}') for i in range(len(amd_header_section))]
   h, bi, a, b = amd.asm.get_assembly()
-  [print(f'Amd Asm: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
+  [print(f'Amd Asm hello: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
+  print('--- amd64 elf header ---')
+def read_arm_macho_func() -> None:
+  print('--- arm64 macho header ---')
+  arm = Assembly(f'{os.path.dirname(os.path.realpath(__file__))}/examples/func_arm64_macho.bin', arch='arm64', flavour='arm64', binfmt='macho')
+  arm_header  = arm.asm.get_header()
+  arm_command = arm.asm.get_command()
+  arm_loader  = arm.asm.get_loader(0)
+  arm_segment = arm.asm.get_segment(80)
+  arm_data    = arm.asm.get_data()
+  arm.print_hex(arm.bytes2hex(arm_header))
+  arm.print_hex(arm.bytes2hex(arm_loader))
+  arm.print_hex(arm.bytes2hex(arm_segment))
+  arm.print_hex(arm.bytes2hex(arm_data))
+  [print(f'Header {arm.hex2str(arm_header[i])}') for i in range(len(arm_header))]
+  [print(f'Loader {arm.hex2str(arm_loader[i])}') for i in range(len(arm_loader))]
+  [print(f'Segment {arm.hex2str(arm_segment[i])}') for i in range(len(arm_segment))]
+  h, bi, a, b = arm.asm.get_assembly()
+  [print(f'Arm Asm func: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
+  print('--- arm64 macho header ---')
+def read_amd_elf_func() -> None:
+  print('--- amd64 elf header ---')
+  amd = Assembly(f'{os.path.dirname(os.path.realpath(__file__))}/examples/func_amd64_elf.bin', arch='amd64', flavour='amd64', binfmt='elf')
+  amd_header         = amd.asm.get_header()
+  amd_header_program = amd.asm.get_header_program()
+  amd_header_section = amd.asm.get_header_section()
+  amd_data           = amd.asm.get_data()
+  amd.print_hex(amd.bytes2hex(amd_header))
+  amd.print_hex(amd.bytes2hex(amd_header_program))
+  amd.print_hex(amd.bytes2hex(amd_header_section))
+  amd.print_hex(amd.bytes2hex(amd_data))
+  [print(f'Header {amd.hex2str(amd_header[i])}') for i in range(len(amd_header))]
+  [print(f'Program {amd.hex2str(amd_header_program[i])}') for i in range(len(amd_header_program))]
+  [print(f'Section {amd.hex2str(amd_header_section[i])}') for i in range(len(amd_header_section))]
+  h, bi, a, b = amd.asm.get_assembly()
+  [print(f'Amd Asm func: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
   print('--- amd64 elf header ---')
 
 
 if __name__ == '__main__':
-  read_arm_macho()
-  read_amd_elf()
+  read_arm_macho_hello()
+  read_arm_macho_func()
+  read_amd_elf_hello()
+  read_amd_elf_func()
   print('OK')
