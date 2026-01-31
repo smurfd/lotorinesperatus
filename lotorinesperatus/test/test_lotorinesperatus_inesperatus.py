93b4445f47c080619a819ab54f36bf5554f9b508
Nicklas Boman <smurfd@gmail.com>
2026-01-31 19:44:24 +0100

    lotorinesperatus, dissasamblin'. amd read assembly the correct way, mostly fixed. still need to read all data

--- lotorinesperatus/test/test_lotorinesperatus_inesperatus.py
+++ lotorinesperatus/test/test_lotorinesperatus_inesperatus.py
@@ -37,12 +37,7 @@ def read_amd_elf_hello() -> None:
   [print(f'Header {amd.hex2str(amd_header[i])}') for i in range(len(amd_header))]
   [print(f'Program {amd.hex2str(amd_header_program[i])}') for i in range(len(amd_header_program))]
   [print(f'Section {amd.hex2str(amd_header_section[i])}') for i in range(len(amd_header_section))]
-  h, bi, a, b = amd.asm.get_assembly(1)
-  [print(f'Amd Asm hello: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
-  print('////')
   print(f'{amd.asm.get_assembly_correctly()}')
-  
-  #print(f'{hex(int.from_bytes(amd.asm.get_assembly_correctly()))}')
   print('--- amd64 elf header ---')
 def read_arm_macho_func() -> None:
   print('--- arm64 macho header ---')
@@ -76,11 +71,7 @@ def read_amd_elf_func() -> None:
   [print(f'Header {amd.hex2str(amd_header[i])}') for i in range(len(amd_header))]
   [print(f'Program {amd.hex2str(amd_header_program[i])}') for i in range(len(amd_header_program))]
   [print(f'Section {amd.hex2str(amd_header_section[i])}') for i in range(len(amd_header_section))]
-  h, bi, a, b = amd.asm.get_assembly(2)
-  [print(f'Amd Asm func: {h[i]} {bi[i]} {a[i]}') for i in range(len(h))]
-  print('////')
   print(f'{amd.asm.get_assembly_correctly()}')
-  #print(f'{hex(int.from_bytes(amd.asm.get_assembly_correctly()))}')
   print('--- amd64 elf header ---')
 def read_helloamd_objdump() -> None:
   hamd = Objdump('lotorinesperatus/test/examples/hello_amd64_elf.objdump').get()
@@ -89,12 +80,10 @@ def read_helloamd_objdump() -> None:
   amd_header_program = amd.asm.get_header_program()
   amd_header_section = amd.asm.get_header_section()
   amd_data           = amd.asm.get_data()
-  h, bi, a, b = amd.asm.get_assembly(1)
   asmc = amd.asm.get_assembly_correctly()
   c = 0
   for i,f in enumerate(hamd):
     if f[0] == 'file format elf64-x86-64': c = -1; continue
-    assert f[0] == a[i+c].split(' ')[0]
     assert f[0][:3] == asmc[i - 1].split(' ')[0][:3]  # Read first 3 letters in op since we have not figured out the sufix, like movQ, movL etc.
     if c < 0 and i > 2: c = 0
 def read_helloarm_objdump() -> None:
@@ -114,12 +103,10 @@ def read_funcamd_objdump() -> None:
   amd_header_program = amd.asm.get_header_program()
   amd_header_section = amd.asm.get_header_section()
   amd_data           = amd.asm.get_data()
-  h, bi, a, b = amd.asm.get_assembly(2)
   asmc = amd.asm.get_assembly_correctly()  # TODO: fetch all data
   c = 0
   for i,f in enumerate(famd):
     if f[0] == 'file format elf64-x86-64': c = -1; continue
-    assert f[0] == a[i+c].split(' ')[0]
     if i > len(asmc): break  # We dont fetch enough
     assert f[0][:3] == asmc[i - 1].split(' ')[0][:3]  # Read first 3 letters in op since we have not figured out the sufix, like movQ, movL etc.
     if c < 0 and i > 2: c = 0
